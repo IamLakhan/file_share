eed527b6bedd7bb2e7902ebc982623bde9d286d0
IamLakhan <gurbanilakhan7@gmail.com>
2021-10-11 14:30:08 +0530

    changes in dashboard, listing out files

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from .models import File
 # Create your views here.
 def first_entry(request):
     if request.user.is_authenticated:
-        return render(request, 'dashboard.html',{'user': User.objects.get(username=request.user.username)})
+        return render(request, 'dashboard.html',{'file': File.objects.filter(user=request.user)})
     return render(request, 'home.html')
