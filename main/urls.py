653f22e6e9112d5f12e599bebe27da0b158ac387
IamLakhan <gurbanilakhan7@gmail.com>
2021-11-24 21:01:01 +0530

    static files added

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import file_upload, first_entry
 
 urlpatterns = [
-    path('file-upload/', file_upload),
+    path('file-upload/', file_upload, name="upload"),
     # path('', first_entry, name = 'home'),
 ]
