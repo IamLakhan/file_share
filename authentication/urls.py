e901f673967953002647767661f99a9a67fb724a
IamLakhan <gurbanilakhan7@gmail.com>
2021-11-24 16:04:43 +0530

    landing and dashboard frontend

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,6 +5,6 @@ from .views import login_user, logout_user, register
 
 urlpatterns = [
     path('login/', login_user, name='login'),
-    path('logout/', logout_user),
+    path('logout/', logout_user, name='logout'),
     path('register/', register, name="register"),
 ]
